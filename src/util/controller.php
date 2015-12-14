93484b1f3d2b18b0c7c8345c51d8bdf91664ec46
Guillaume Pierre-Antoine <pierreantoine.guillaume+github@gmail.com>
2015-12-14 15:45:51 +0100

    controller maquette

---
+++ src/util/controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: g20901528
+ * Date: 14/12/15
+ * Time: 15:42
+ */
+
+trait controller {
+    function getRoute() {
+        return this->route;
+    }
+}
