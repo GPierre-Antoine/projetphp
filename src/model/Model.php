7b2405fa73568eb25032cfbfef5d00dcaf8ef1d2
kevin0496 <kevin13500@hotmail.fr>
2015-12-14 15:54:28 +0100

    create dir model view controller

---
+++ src/model/Model.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: h13002021
+ * Date: 14/12/15
+ * Time: 15:53
+ */
+
+trait Model {
+
+}
---
+++ src/view/View.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: h13002021
+ * Date: 14/12/15
+ * Time: 15:54
+ */
+
+trait View {
+
+}
