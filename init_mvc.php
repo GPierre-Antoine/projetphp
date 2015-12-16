81e6c1a5d48bdbb74cc84de5c2d572d9b2d16256
PAG <pierreantoine.guillaume+github@gmail.com>
2015-12-16 17:40:31 +0100

    mvc update

--- init_mvc.php
+++ init_mvc.php
@@ -10,9 +10,15 @@ $task = preg_match("/^\/?([a-zA-Z0-9\-\_]+)\/(.*)/",$path,$matches);
 
 $route = $router->getRoute($matches[1]);
 
+
+
+echo "Controller : $route->controller <br /> Model : $route->model <br /> View : $route->view";
+
+/*
 $model = new $route->model;
 $controller = new $route->controller($model,$matches[2]);
 $view = new $route->view($model);
 
 $controller->update();
 $view->display();
+*/
