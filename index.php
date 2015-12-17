bfe6f20edf66729d84a8e58c2ff9b057f91505ff
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-12-18 00:32:10 +0100

    comments of index.php

--- index.php
+++ index.php
@@ -1,32 +1,53 @@
 <?php
 //$sessionStart = session_start();
+
+//initialization of arrays containing pathname from classname
 include_once("src/util/mvc_catcher.php");
-include_once("src/util/db_wrap.php");
-include_once("src/Router.php");
+
+//initialization of database
 include_once("src/util/db_wrap.php");
 
 $connexion = new \db\db_handler();
 $connexion->init();
 
+//Routing initialization
+include_once("src/Router.php");
+
 $router = new Router();
+
+
+//URL rewriting
 $path = $_SERVER["REQUEST_URI"];
 
+//$matches[1] contains the first tokken in the url until the first non alphanum
+//$matches[2] contains all the rest
 $matches = array();
 $task = preg_match("/^\/?([a-zA-Z0-9\-\_]+)\/?(.*)$/",$path,$matches);
 
+
+//get the right route from the first parameter
 $route = $router->getRoute($matches[1]);
 
+//include according files
 include_once($models[$route->model]);
 include_once($controllers[$route->controller]);
 include_once($views[$route->view]);
 
+
+//get correct MVC bloc
 $model = new $route->model;
 $controller = new $route->controller($model,$matches[2]);
 $view = new $route->view($model);
 
-$controller->update();
-$view->display();
 
+try {
+    $controller->update();
+    $view->display();
+} catch (exception $e) {
+
+}
+
+//close db connexion
 $connexion->close();
 $connexion = null;
 
