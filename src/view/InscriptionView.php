dd9a85b72a55a9abf635a548aefe33d082c77768
Scyar <enzo_alex@hotmail.fr>
2015-12-23 21:40:28 +0100

    Test Git

--- src/view/InscriptionView.php
+++ src/view/InscriptionView.php
@@ -47,7 +47,7 @@ class InscriptionView extends View {
         		    	</div>
         		    </div>
         		    <div class='section' data-anchor='propos' id='propos'>
-        		    	Coucou
+        		    	Coucouc
         		    </div>
         		</div>
           </body>
