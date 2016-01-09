71b698752b6322395a037853adec52ae82f530b3
h13002021 <kevin.hoarau@etu.univ-amu.fr>
2016-01-09 13:24:51 +0100

    SETTINGS BE CONTINUED

--- src/view/SettingsView.php
+++ src/view/SettingsView.php
@@ -31,6 +31,18 @@ class SettingsView extends View {
 						<img class="top_logo" src="/src/images/aaron_logo.png">
 						<span class="top_username"><strong>'.$this->user->getName().'</strong></span>
 					</div>
+
+					<div id="body">
+					    <div id="param_account" class="param_box">
+
+					    </div>
+					    <div id="param_mail" class="param_box">
+
+					    </div>
+					    <div id="param_pwd" class="param_box">
+
+					    </div>
+					</div>
 				</body>
 			</html>
 		';
