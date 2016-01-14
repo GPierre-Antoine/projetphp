6ec0dd3d63fd1087a7338e8fab29b22923a567fd
Nosmoz <romain.goffi@free.fr>
2016-01-14 10:08:33 +0100

    MODYFY - CONFIRMATIONMODEL / CONFIRMATIONCONTROLLER

--- src/model/ConfirmationModel.php
+++ src/model/ConfirmationModel.php
@@ -18,6 +18,16 @@ class ConfirmationModel extends ModelPDO {
 
     }
 
+    public function recup_key_inscription ($id) {
+        $this->pdo->prepare("SELECT TOKEN,ACTIF FROM VERIFICATION WHERE ID LIKE :ID");
+        $this->pdo->execute(array(":ID" => $id));
+    }
+
+    public function validate_inscription() {
+        $this->pdo->prepare("UPDATE VERIFICATION SET ACTIF = 1 WHERE ID LIKE :ID");
+        $this->pdo->execute(array());
+    }
+
     public function get () {
 
     }
