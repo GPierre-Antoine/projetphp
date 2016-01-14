cf4ebc5f6e86330bc2dff204912af61c0e95eb0e
g20901528 <pierreantoine.guillaume+github@gmail.com>
2016-01-14 10:11:02 +0100

    umodel

--- src/util/encryption.php
+++ src/util/encryption.php
@@ -4,6 +4,6 @@ function encrypt ($password,$token) {
     echo "<br />encrypt :<pre>";
     var_dump(func_get_args());
     echo "</pre>";
-    $result = $password;//hash('sha512',$password);
-    return $result.'1';
+    $result = hash('sha512',$password.$token);
+    return $result;
 }
