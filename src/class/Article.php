e8b3b313fc73e148668ac5369c02c98bb6ac4b26
Kevin HOARAU <Kevin HOARAU>
2016-01-21 23:18:24 +0100

    MASSIVE UPDATE DESIGN/OPTIMISE

--- src/class/Article.php
+++ src/class/Article.php
@@ -15,14 +15,16 @@ class Article
     private $imgUrl;
     private $content;
     private $date;
+    private $source;
 
-    public function __construct($id, $title, $theme, $imgUrl, $content, $date) {
+    public function __construct($id, $title, $theme, $imgUrl, $content, $date, $source) {
         $this->id = $id;
         $this->title = $title;
         $this->theme = $theme;
         $this->imgUrl = $imgUrl;
         $this->content = $content;
         $this->date = $date;
+        $this->source = $source;
     }
 
     public function display() {
@@ -33,6 +35,7 @@ class Article
                 </div >
                 <div class="article_zone_content" >
                     <span class="article_content_inf"><span class="article_inf_title">'.$this->title.'</span> dans <span class="article_inf_theme">'.$this->theme.'</span></span><span class="article_inf_date">le '.$this->date.'</span><br/>
+                    <span class="article_content_source">Par : '.$this->source.'</span>
                     <p class="article_content">'.$this->content.'</p>
                 </div>
             </div>
--- src/js/switch_content.js
+++ src/js/switch_content.js
@@ -7,14 +7,31 @@ $(function () {
     var contentFlux = true;
     var contentBlog = false;
     var contentMail = false;
+    var contentFriendBlog = false;
 
     $(".blog_btn").click(function() {
         if (!contentBlog) {
             $("#content_mail").addClass("hide");
+            $("#content_friend_blog").addClass("hide");
             $("#content_flux").addClass("hide");
             $("#content_blog").removeClass("hide");
 
             contentBlog = true;
+            contentFlux = false;
+            contentMail = false;
+            contentFriendBlog = false;
+        }
+    })
+
+    $(".blog_friend_btn").click(function() {
+        if (!contentFriendBlog) {
+            $("#content_mail").addClass("hide");
+            $("#content_flux").addClass("hide");
+            $("#content_blog").addClass("hide");
+            $("#content_friend_blog").removeClass("hide");
+
+            contentFriendBlog = true;
+            contentBlog = false;
             contentFlux = false;
             contentMail = false;
         }
@@ -23,36 +40,42 @@ $(function () {
     $(".actu_btn").click(function() {
         if (!contentFlux) {
             $("#content_blog").addClass("hide");
+            $("#content_friend_blog").addClass("hide");
             $("#content_mail").addClass("hide");
             $("#content_flux").removeClass("hide");
 
             contentFlux = true;
             contentBlog = false;
             contentMail = false;
+            contentFriendBlog = false;
         }
     })
 
     $(".mail_btn").click(function() {
         if (!contentMail) {
             $("#content_blog").addClass("hide");
+            $("#content_friend_blog").addClass("hide");
             $("#content_flux").addClass("hide");
             $("#content_mail").removeClass("hide");
 
             contentMail = true;
             contentFlux = false;
             contentBlog = false;
+            contentFriendBlog = false;
         }
     })
 
     $("#add_article").click(function () {
         if (!contentBlog) {
             $("#content_flux").addClass("hide");
+            $("#content_friend_blog").addClass("hide");
             $("#content_mail").addClass("hide");
             $("#content_blog").removeClass("hide");
 
             contentBlog = true;
             contentFlux = false;
             contentMail = false;
+            contentFriendBlog = false;
         }
     });
 
