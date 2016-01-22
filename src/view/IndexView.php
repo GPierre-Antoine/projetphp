105f80b48ce926731df96a75381e72137ae652fb
Nosmoz <romain.goffi@free.fr>
2016-01-22 09:30:05 +0100

    add href on 'Mot de passe perdu'

--- src/view/IndexView.php
+++ src/view/IndexView.php
@@ -69,7 +69,7 @@ class IndexView extends View {
         			    			<input class='smallInput' name='mail' type='email' placeholder='Adresse Mail'/>
         			    			<input class='smallInput' name='pwd' type='password' placeholder='Mot de Passe'/>
         			    			<input id='submitIndex' name='action' type='submit' value='Rentrer sur Aaron'/>
-        			    			<input id='mdpLostIndex' type='button' name='mdpLost' value=\"Mot de passe perdu ?\" onclick=self.location.href='' />
+        			    			<input id='mdpLostIndex' type='button' name='mdpLost' value=\"Mot de passe perdu ?\" onclick=self.location.href='reset' />
         			    		</form>
         			    	</div>
         		    	</div>
