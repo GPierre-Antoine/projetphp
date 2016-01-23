43a4a8b75a11c5887a243ba81eda543d821452c5
Guillaume Pierre-Antoine <pierreantoine.guillaume+github@gmail.com>
2016-01-23 13:50:35 +0100

    strategy => added / to path

--- src/class/RequestStrategy.php
+++ src/class/RequestStrategy.php
@@ -13,7 +13,7 @@ class RequestStrategy extends StrategyResetPassword{
         echo <<<TEXT
 
         <p> Veuillez ajouter votre adresse mail afin d'obtenir un nouveau mot de passe</p>
-        			    <form action="reset" method="post">
+        			    <form action="/reset" method="post">
         			    	<input class="bigInput" name="password" type="password" placeholder="Mot de passe" required/><br/>
         			    	<input class="bigInput" name="password" type="password" placeholder="Verification mot de passe" required/><br/>
         			    	<input id="submitIndex" name="action" type="submit" value="Valider" />
